1ec6191e1f8bd0c5cbd1743e1a781ef8b65386a9
Priyanka Rani <priyanka.rani@openlabs.co.in>
2013-08-28 16:46:34 +0530

    Migrate to 2.8 #2346
    Review ID: 415001

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ major_version, minor_version, _ = info.get('version', '0.0.1').split('.', 2)
 major_version = int(major_version)
 minor_version = int(minor_version)
 
-requires = ['flask_oauth', 'flask<0.10']
+requires = ['flask_oauth']
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
         requires.append(
