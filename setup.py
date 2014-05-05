8ef129353c608a3ffaf3f92fa85c3caf1d2e187b
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-05-05 07:34:21 +0530

    Future proof the nereid version dependency

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ minor_version = int(minor_version)
 
 requires = [
     'flask_oauth',
-    'trytond_nereid>=3.0.7.0',
+    'trytond_nereid>=3.0.7.0,<3.1',
 ]
 for dep in info.get('depends', []):
     if not re.match(r'(ir|res|webdav)(\W|$)', dep):
